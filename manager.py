9d83f9ccdb68432b817bc46c41b606625d3c9ccf
Pedro Queiros <pqueiros@gmail.com>
2013-06-11 01:19:36 +0200

    hell, complete poc is done all adge cases should be covered now
    TODO
    check todos
    and re-analyse the code
    
    somehow dont like it so much

---
+++ manager.py
@@ -0,0 +1,119 @@
+import guiManager as guiM
+import threading
+import time
+
+class RunState:
+	WARNING = "warning"
+	FAILED  = "failed"
+	SUCCESS = "success"
+
+class Plugin():
+	
+	def __init__(self, id):
+		self.id = id
+
+	def run(self):
+		msg = "Default plugin id:" + str(self.id) + " run call executed -- Does nothing"
+		print msg
+		return RunState.SUCCESS, msg
+
+class DebugPlugin(Plugin):
+	def __init__(self, id, returnCode, message):
+		Plugin.__init__(self,id)
+		self.__retCode = returnCode
+		self.__msg = message
+
+	def run(self):
+		msg = "DebugPlugin:\n" + self.__msg 
+		return self.__retCode,  msg
+
+
+class PluginManager(threading.Thread):
+	def __init__(self, plugins, gui):
+		threading.Thread.__init__(self)
+		self.__plugins = plugins
+		self.__gui = gui
+		self.__run = False
+		self.__info_map = {
+		RunState.WARNING: guiM.InfoFormat.CONFIRMATION
+		,RunState.FAILED: guiM.InfoFormat.ERROR
+		,RunState.SUCCESS: guiM.InfoFormat.NONE
+		}
+
+	def run(self):
+		interrupted = False
+		for pg in self.__plugins:
+			if not self.__run_plugin(pg):
+				interrupted = True
+				break
+
+		if not interrupted:
+			print "Everything done, sleep a bit & stop Gui"
+			time.sleep(5)
+			self.__gui.stop()
+			print "gui stopped, run says goodby"
+		print "run Done"
+
+
+	def __run_plugin(self, pg):
+		result = False
+		try:
+			prefix = "Plugin-{id}: ".format(id=pg.id)
+			self.__gui.updateUserInfo(prefix+"Starting...\n")
+			
+			state, pg_out = pg.run()	
+
+			info = prefix + str(pg_out) + "\n"	
+			self.__gui.updateUserInfo(info, self.__info_map[state])
+
+			self.__gui.updateUserInfo(prefix+"Done.\n")
+
+			result = (state == RunState.SUCCESS)
+
+			if state == RunState.WARNING:
+		 		result = self.__gui.waitUserConfirmation()
+		except guiM.GUITerminating:
+		 	print "gui terminating"
+		return result
+
+def main():
+	gui = guiM.GUIManager()
+
+	conf_plugin = DebugPlugin("confirmation_plugin", RunState.WARNING,
+		"""
+		This should have different color
+		please Cancel to stop or Confirm to continue
+		"""
+		)
+
+	failed_plugin = DebugPlugin("Failure_plugin", RunState.FAILED,
+		"""
+		This should have different color
+		plugin failed execution and user can only press cancel
+		to abort execution
+		No other plugin is executed after a failed plugin
+		"""
+		)
+
+	plugins = [conf_plugin]
+	for id in range(1,50):
+		plugins.append(Plugin(id))
+
+	plugins.append(conf_plugin)
+
+	plugins.append(Plugin("Last_plugin"))
+
+	plugins.append(failed_plugin)
+
+	plugin_manager = PluginManager(plugins, gui)
+
+	plugin_manager.start()
+
+	gui.main_loop()
+
+	plugin_manager.join()
+
+	print "Done"
+
+if __name__ == '__main__':
+    main()
